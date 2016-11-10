9b3e56e9866b76b36be4f7a1b469fa1f704e8cf0
odalton <oliver.dalton.au@gmail.com>
2016-11-10 18:21:59 +0800

    remmed out missing import

--- js/sw/index.js
+++ js/sw/index.js
@@ -1,6 +1,6 @@
 // Chrome's currently missing some useful cache methods,
 // this polyfill adds them.
-importScripts('serviceworker-cache-polyfill.js');
+//importScripts('serviceworker-cache-polyfill.js'); // this dont exist
 
 // Here comes the install event!
 // This only happens once, when the browser sees this
