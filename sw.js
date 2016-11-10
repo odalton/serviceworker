64beb856bcbde91ff865a1c88a197515d70f0ad2
odalton <oliver.dalton.au@gmail.com>
2016-11-10 19:04:44 +0800

    Added simple sw and updated html to target the new sw

---
+++ sw.js
@@ -0,0 +1,9 @@
+// if the browser supports a sw
+if (navigator.serviceWorker) {
+    log("Browser supports Service Worker");
+    if (navigator.serviceWorker.current) {
+        log("Current Service Worker state: \\o/");
+        log('Go to chrome://serviceworker-internals/ or about:serviceworkers to see Service Worker debug output');
+    } else {
+        log("No Service Worker active...");
+    }
